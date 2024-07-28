8ce4a27e7cef1957013b3146a3a6d2423ad42287
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-07-28 16:00:21 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './App.css'
-import { useSelector , useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './redux/slices/counter'
 
 const App = () => {
   const count = useSelector((state) => state.counter)
-  const counts = useSelector((s)=> s.words)
+  const counts = useSelector((s) => s.words)
   const dispatch = useDispatch()
-  
+
   return (
     <div>
       <h1>Count is {count}</h1>
       <h1>{counts}</h1>
-      <button onClick={()=> dispatch(increment())}>Increment</button>
-      <button onClick={()=> dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   )
 }
