bca97210e24480075582ba6d70e4d60c1826661c
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-07-28 15:59:40 +0530

    new

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import counterSlice from './slices/counter'
 import counterWords from './slices/worder'
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 
 const store = configureStore({
-    reducer : {
-        counter : counterSlice,
-        words : counterWords,
+    reducer: {
+        counter: counterSlice,
+        words: counterWords,
     },
 })
 
